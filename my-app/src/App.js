ce0b53a438af587a2d223b37861b96d2bfe4c41a
Adrian Sequeira <adrian.sequeira@outlook.com>
2024-05-29 15:59:56 -0600

    Installing a react framework and wallet connect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { StrictMode } from 'react';
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";  
+import Login from './components/Login/Login.js'
 
-function App() {
+export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StrictMode>
+      <PrimeReactProvider>
+        <Login/>
+      </PrimeReactProvider>
+    </StrictMode>
   );
 }
-
-export default App;
